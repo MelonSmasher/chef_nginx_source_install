0b4bf9756c589066cd316c42b2d86274161d2f73
Alex Markessinis <markea@sage.edu>
2017-02-04 14:15:08 -0500

    fixed package info

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# nginx_source_install CHANGELOG
-
-This file is used to list changes made in each version of the nginx_source_install cookbook.
-
-## 0.1.0
-- [Alex Markessinis] - Initial release of nginx_source_install
--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,9 @@ maintainer_email 'markea125@gmail.com'
 license          'MIT'
 description      'Installs/Configures Nginx from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+issues_url 'https://github.com/MelonSmasher/chef_nginx_source_install/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/MelonSmasher/chef_nginx_source_install' if respond_to?(:source_url)
+%w( centos debian redhat suse ubuntu ).each do |os|
+  supports os
+end
