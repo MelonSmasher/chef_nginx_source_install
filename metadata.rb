b29600b7052363ba8bd2a90ff597e64ac3349ebb
Alex Markessinis <markea@sage.edu>
2017-02-04 14:24:21 -0500

    removed suse from supported os

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'markea125@gmail.com'
 license          'MIT'
 description      'Installs/Configures Nginx from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 chef_version "~> 12"
 issues_url 'https://github.com/MelonSmasher/chef_nginx_source_install/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_nginx_source_install' if respond_to?(:source_url)
-%w( centos debian redhat suse ubuntu ).each do |os|
+%w( centos debian redhat ubuntu ).each do |os|
   supports os
 end
