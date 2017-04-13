17ca20fa65266540513c28226ac832fa0fb37a96
Alex Markessinis <markea@sage.edu>
2017-04-13 10:55:12 -0400

    fixed required chef version

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'markea125@gmail.com'
 license          'MIT'
 description      'Installs/Configures Nginx from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
-chef_version "~> 12"
+version          '1.0.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/MelonSmasher/chef_nginx_source_install/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_nginx_source_install' if respond_to?(:source_url)
 %w( centos debian redhat ubuntu ).each do |os|
